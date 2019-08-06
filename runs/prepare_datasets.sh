dbe6a236f87c94873d11465e7cf1150159be217d
Yongchan Kwon <ykwon0407@snu.ac.kr>
2019-08-06 16:54:31 +0900

    yml

--- runs/prepare_datasets.sh
+++ runs/prepare_datasets.sh
@@ -9,7 +9,7 @@ CUDA_VISIBLE_DEVICES=0 python3 ./input/create_datasets.py cifar10 cifar100
 for seed in 1 2 3 4 5; do
     for size in 1000 2500 5000 25000 50000; do
         CUDA_VISIBLE_DEVICES=0 python3 ./input/create_split.py --seed=$seed --size=$size ./input/cifar10/cifar10 ./input/cifar10-train.tfrecord &
-        CUDA_VISIBLE_DEVICES=1 python3 ./input/create_split.py --seed=$seed --size=$size ./input/cifar100/cifar100 ./input/cifar100-train.tfrecord &
+        CUDA_VISIBLE_DEVICES=1 python3 ./input/create_split.py --seed=$seed --size=$size ./input/cifar100/cifar100 ./input/cifar100-train.tfrecord
     done
     for size in 100 500; do
         CUDA_VISIBLE_DEVICES=0 python3 ./input/create_split.py --seed=$seed --size=$size ./input/cifar10/cifar10 ./input/cifar10-train.tfrecord
