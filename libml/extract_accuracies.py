05d2ec77512a498479edef8f339c591a963d686b
Yongchan Kwon <ykwon0407@snu.ac.kr>
2019-08-22 14:26:40 +0900

    extract

---
+++ libml/extract_accuracies.py
@@ -0,0 +1,13 @@
+from glob import glob
+import sys, os
+
+experiment_directory = '/data/dro-id_history'
+new_directory ='../experiments/acc_all'
+if not os.path.exists(new_directory):
+	os.makedirs(new_directory)
+
+for from_directory in sorted(glob(experiment_directory+'/*/*/*')):
+	to_directory = new_directory+'{}'.format(from_directory[len(experiment_directory):])
+	if not os.path.exists(to_directory):
+		os.makedirs(to_directory)
+		os.system('cp {} {}'.format(from_directory+'/accuracies.txt', to_directory+'/accuracies.txt'))
