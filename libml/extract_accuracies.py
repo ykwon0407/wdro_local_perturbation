b22a4f7550507ea96321a5747496f2ce45e1fa5f
WY <eraser347@snu.ac.kr>
2019-08-27 15:39:02 +0900

    l2 plot

--- libml/extract_accuracies.py
+++ libml/extract_accuracies.py
@@ -7,7 +7,7 @@ new_directory ='./experiments/acc_all'
 if not os.path.exists(new_directory):
 	os.makedirs(new_directory)
 
-for from_directory in sorted(glob(experiment_directory+'/max*/*/*')):
+for from_directory in sorted(glob(experiment_directory+'/l2*/*/*')):
 	to_directory = new_directory+'{}'.format(from_directory[len(experiment_directory):])
 	if not os.path.exists(to_directory):
 		os.makedirs(to_directory)
