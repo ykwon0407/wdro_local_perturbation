1a8d6e5a2891c359178e04524c29b9858e9853cf
WY <eraser347@snu.ac.kr>
2019-08-27 13:33:50 +0900

    plot for l2

--- libml/extract_accuracies.py
+++ libml/extract_accuracies.py
@@ -1,12 +1,13 @@
 from glob import glob
 import sys, os
 
-experiment_directory = '/data/dro-id_history'
+experiment_directory = './data/dro-io_history'
 new_directory ='../experiments/acc_all'
 if not os.path.exists(new_directory):
 	os.makedirs(new_directory)
 
 for from_directory in sorted(glob(experiment_directory+'/*/*/*')):
+	print(from_directory)
 	to_directory = new_directory+'{}'.format(from_directory[len(experiment_directory):])
 	if not os.path.exists(to_directory):
 		os.makedirs(to_directory)
