622dcc0f5dbf08f1ac660ad3507f23d4c99ead9c
WY <eraser347@snu.ac.kr>
2019-09-04 14:58:00 +0900

    train_dir example.sh

--- runs/train_dir_example.sh
+++ runs/train_dir_example.sh
@@ -5,23 +5,22 @@ cd ~/deep/project/dro-id/
 
 #train_dir: directory to save the model folder (such as erm_ema, l2_0.0001, maxsup_2, mixup ...)
 #default train dir is ./experiments/
-#Directory is saved as os.path.join(train_dir, model_dir with hyperparameters, dataset.${seed}@${size}-1,
-## -example: ./experiments/l2_0.0001/cifar100.1@50000-1/tf, args  
+
+#'tf' and 'args' are saved at: os.path.join(train_dir, model_dir with hyperparameters, dataset.${seed}@${size}-1)
+####example: ./experiments/l2_0.0001/cifar100.1@50000-1/tf, args  
 
 #number of epochs
 $common_args = --nepoch 100 
+
 #erm
-CUDA_VISIBLE_DEVICES=0 python3 erm.py --dataset=cifar10.${seed}@${size}-1 --wd=0.02 --smoothing 0.001 $common_args
-#
-CUDA_VISIBLE_DEVICES=0 python3 erm.py --dataset=cifar10.${seed}@${size}-1 --wd=0.02 --smoothing 0.001 $common_args
+CUDA_VISIBLE_DEVICES=0 python3 erm.py --dataset=cifar10.1@50000-1 --wd=0.02 --smoothing 0.001 $common_args
+#### saved at ./experiments/erm/cifar10.1@50000-1/tf, args
 
+#mixup
+CUDA_VISIBLE_DEVICES=0 python3 mixup_grad.py --dataset=cifar100.5@5000-1 $common_args
+#### saved at ./experiments/mixup/cifar100.5@5000-1/tf, args
 
-#Run data with CUDA 0~1
-for seed in 1 2 3 4 5; do
-    for size in 1000 2500 5000 25000 50000; do
-    
-    CUDA_VISIBLE_DEVICES=1 python3 erm.py --train_dir experiments/erm100@${size} --dataset=cifar100.${seed}@${size}-1 --wd=0.02 --smoothing 0.001 $common_args &
-    CUDA_VISIBLE_DEVICES=2 python3 mixup.py --train_dir experiments/mixup10@${size} --dataset=cifar10.${seed}@${size}-1 $common_args &
-    CUDA_VISIBLE_DEVICES=3 python3 mixup.py --train_dir experiments/mixup100@${size} --dataset=cifar100.${seed}@${size}-1 $common_args
-    done
-done
+#l2
+CUDA_VISIBLE_DEVICES=0 python3 mixup_grad.py --dataset=cifar100.5@5000-1 --regularizer l2 --gamma 0.1234 $common_args
+#### saved at ./experiments/l2_0.1234/cifar100.5@5000-1/tf, args
+
